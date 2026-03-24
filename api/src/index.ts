94e31e935f2e8866a28771c52a9b42400d517eac
ezms <emanuelm.dev@gmail.com>
2026-03-24 13:40:32 -0300

    feat: set env to dynamic URL :technologist:

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,13 +2,18 @@ import { serve } from '@hono/node-server';
 import app from './app';
 import { loadRoutes } from './routes';
 
+const PORT = Number(process.env.PORT) || 3001;
+const HOST = process.env.HOST || 'localhost';
+const PROTOCOL = process.env.PROTOCOL || 'http';
+
 const HONO_SERVER_OPTIONS = {
     fetch: app.fetch,
-    port: 3000,
+    port: PORT,
+    hostname: HOST,
 };
 
 loadRoutes(app);
 
 serve(HONO_SERVER_OPTIONS, (info) => {
-    console.log(`\u{1F680} Server is running on http://localhost:${info.port}`);
+    console.log(`\u{1F680} Server is running on ${PROTOCOL}://${info.address}:${info.port}`);
 });
